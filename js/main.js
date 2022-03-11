790f5b8de0e90a6ca3d52ba484d42f3e2cf21e6c
neogmanish99 <sanjuneog399@gmail.com>
2022-03-11 20:42:06 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+    // Smooth Scrolling
+    $('#navbar a, .btn').on('click', function(e) {
+        if (this.hash !== '') {
+            e.preventDefault();
+            
+            const hash = this.hash;
+            
+            $('html, body').animate(
+            {
+            scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+        }
+    });
+
+    
+    // Sticky menu background
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.8;
+        } else {
+        document.querySelector('#navbar').style.opacity = 1;
+        }
+    });
